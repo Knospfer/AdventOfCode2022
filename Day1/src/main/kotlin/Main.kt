f52a718f97f35af49e35ecbbdbb09e68f83ff7d6
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-12-02 21:20:01 +0100

    feat: day one

--- Day1/src/main/kotlin/Main.kt
+++ Day1/src/main/kotlin/Main.kt
@@ -1,7 +1,39 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("Hello Day One!")
+
+    println("---- Part One ----")
+    val calories = parseFileIntoTotalCaloriesList()
+    println("Maximum calories value: ${calories.max()}")
+
+    println("---- Part Two ----")
+    val sortedCalories = calories.sortedDescending()
+    val first = sortedCalories[0]
+    val second = sortedCalories[1]
+    val third = sortedCalories[2]
+    println(
+        "Top 3 calories value: \n" +
+                "1. $first\n" +
+                "2. $second\n" +
+                "3. $third\n" +
+                "Total: ${first + second + third}"
+    )
+}
+
+fun parseFileIntoTotalCaloriesList(): MutableList<Int> {
+    val calories = mutableListOf(0)
+
+    File("src/main/resources/calories.txt").useLines {
+        it.toList().forEach { value ->
+            if (value == "") calories.add(0)
+            else {
+                var currentCalory = calories.last()
+                currentCalory += value.toInt()
+                calories[calories.size - 1] = currentCalory
+            }
+        }
+    }
+
+    return calories
 }
